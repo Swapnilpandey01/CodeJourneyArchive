83bf7d551dea6cef9c7c4ebf5c9dc54a0847881b
Swapnilpandey01 <swapnilpandey0505@gmail.com>
2024-02-19 15:30:15 +0530

    pattern

---
+++ Java/loops.java
@@ -0,0 +1,12 @@
+public class loops {
+    public static void main(String[] args) {
+    for(int j=0;j<4;j++){
+        for(int i=0;i<4;i++){
+            System.out.print('*');
+            System.out.print("  ");
+        }
+        System.out.println();
+    }
+
+    }
+}
